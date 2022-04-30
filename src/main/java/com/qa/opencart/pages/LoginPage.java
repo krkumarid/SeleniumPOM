f4c0182d9f160e1dd00ca284089abf7c9585af5a
Rajeevkumar K <krkumarid@gmail.com>
2022-04-30 15:24:50 -0400

    Added CartPage and uppdated LoginPage.java

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -17,14 +17,14 @@ public class LoginPage
     // 1.Declare private driver
     private WebDriver driver;
     private ElementUtil eleUtil;
-    
+
     // 2.Page Constructor
     public LoginPage(WebDriver driver)
     {
         this.driver = driver;
         eleUtil = new ElementUtil(this.driver);
     }
-
+    
     // 3.By Locators
     private By emaiId = By.id("input-email");
     private By password = By.id("input-password");
@@ -32,62 +32,62 @@ public class LoginPage
     private By registerLink = By.linkText("Register");
     private By forgotPwdLink = By.linkText("Forgotten Password");
     private By warningMessge = By.cssSelector("div.alert.alert-danger.alert-dismissible");
-
+    
     public By getEmaiId()
     {
         return emaiId;
     }
-
+    
     public void setEmaiId(By emaiId)
     {
         this.emaiId = emaiId;
     }
-
+    
     public By getPassword()
     {
         return password;
     }
-
+    
     public void setPassword(By password)
     {
         this.password = password;
     }
-
+    
     public By getLoginButton()
     {
         return LoginButton;
     }
-
+    
     public void setLoginButton(By loginButton)
     {
         LoginButton = loginButton;
     }
-
+    
     // 4.Page Actions
-    @Step("Getting login page title...")
+    @Step("Getting login page title  Value...")
     public String getLoginPageTitle()
     {
         return eleUtil.doGetTitle(ConstantValues.LOGIN_PAGE_TITLE, ConstantValues.DEFAULT_TIME_OUT);
     }
-
+    
     @Step("Getting login page url...")
     public boolean getLoginPageUrl()
     {
         return eleUtil.waitForURLToBe(ConstantValues.LOGIN_PAGE_URL_FRACTION, ConstantValues.DEFAULT_TIME_OUT);
     }
-
+    
     @Step("Checking forgot password link exists  or not..")
     public boolean isForgotPwdLinkExists()
     {
         return eleUtil.doDisplayed(forgotPwdLink);
     }
-
+    
     @Step("Checking register link exists or not...")
     public boolean isRegisterLinkExists()
     {
         return eleUtil.doDisplayed(registerLink);
     }
-
+    
     @Step("do login with username:{0} and password:{1}")
     public AccountPage doLogin(String userName, String pwd)
     {
@@ -97,7 +97,7 @@ public class LoginPage
         eleUtil.doClick(LoginButton);
         return new AccountPage(driver);
     }
-
+    
     @Step("do login with wrong username:{0} and wrong password:{1}")
     public boolean doLoginWithWrongCredentials(String userName, String pwd)
     {
@@ -114,7 +114,7 @@ public class LoginPage
         }
         return true;
     }
-
+    
     @Step("Navigating to the registration page")
     public RegisterAccountPage goToRegisterPage()
     {
