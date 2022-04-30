9dd4b679faa8ce998be09648328ba72f8eba022d
Rajeevkumar K <krkumarid@gmail.com>
2022-04-30 16:32:54 -0400

    Updated OrdePage feature

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,10 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage
 {
     String order;
+    private By check = By.id("orderid");
     
-    public void order()
+    public void orderProduct()
     {
         System.out.println("Order id is 1234");
     }
