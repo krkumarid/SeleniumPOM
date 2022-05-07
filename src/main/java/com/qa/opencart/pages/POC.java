b7e671a93a3d069b66b30a5b8ba4b420aa657952
Rajeevkumar K <krkumarid@gmail.com>
2022-05-07 12:19:41 -0400

    stash test

--- src/main/java/com/qa/opencart/pages/POC.java
+++ src/main/java/com/qa/opencart/pages/POC.java
@@ -2,14 +2,11 @@ package com.qa.opencart.pages;
 
 public class POC
 {
-
+    
     public static void main(String[] args)
     {
-        int i = 10;
-        int b = 20;
-        int sum = i + b;
-      String name = "POC";
-        String ID="1806";
+        String name = "POC";
+        String ID = "1806";
     }
-
+    
 }
