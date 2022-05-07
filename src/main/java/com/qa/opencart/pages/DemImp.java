729509c846a30e1ef5c15e6da25529ba748a0939
Rajeevkumar K <krkumarid@gmail.com>
2022-05-07 11:48:10 -0400

    Ressolved conflict

--- src/main/java/com/qa/opencart/pages/DemImp.java
+++ src/main/java/com/qa/opencart/pages/DemImp.java
@@ -2,18 +2,14 @@ package com.qa.opencart.pages;
 
 public class DemImp
 {
-<<<<<<< HEAD
-    
+
     public void order()
     {
+        /**
+         * Conflicts resolved
+         */
         String name = "Rajeevkumar";
-=======
-
-     public void order()
-    {
-        String name = "Sreesha Rajeevkumar";
-        int total = 100;
->>>>>>> 49852b5c568b8fb469dbadcdd2231c2194bee9af
+        int p = 100;
+        int id = 102;
     }
-
 }
