026a4596e9b8b42ab9bea0f525d95246eb94f85d
Rajeevkumar K <krkumarid@gmail.com>
2022-05-07 14:21:13 -0400

    Reset ftom remote site

--- src/main/java/com/qa/opencart/pages/Cart.java
+++ src/main/java/com/qa/opencart/pages/Cart.java
@@ -2,13 +2,14 @@ package com.qa.opencart.pages;
 
 public class Cart
 {
-
+    
     public static void main(String[] args)
     {
         /**
          * This class is for reset demo from GIT( Remote site
          */
         int i = 10;
+        String str = "KAsi";
     }
-    
+
 }
