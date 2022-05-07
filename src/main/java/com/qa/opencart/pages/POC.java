9a9cf803bb2c1c856e81dc9a9cfc67cbac056fae
Rajeevkumar K <80396431+krkumarid@users.noreply.github.com>
2022-05-07 12:01:36 -0400

    Create POC.java

---
+++ src/main/java/com/qa/opencart/pages/POC.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class POC
+{
+
+    public static void main(String[] args)
+    {
+        int i = 10;
+        int b = 20;
+        int sum = i + b;
+      String name = "POC"
+        String ID="1806";
+    }
+
+}
