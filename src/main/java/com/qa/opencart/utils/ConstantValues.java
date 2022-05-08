68de7ffb9774ec7af7c9111bdc3cb37f5782ea51
Rajeevkumar K <krkumarid@gmail.com>
2022-05-08 12:27:23 -0400

    Default timeout time modified to 7

--- src/main/java/com/qa/opencart/utils/ConstantValues.java
+++ src/main/java/com/qa/opencart/utils/ConstantValues.java
@@ -17,7 +17,7 @@ public class ConstantValues
     public static final String SEARCH_PAGE_URL = "route=product/search";
     public static final String LOGIN_ERROR_MESSG = "No match for E-Mail Address and/or Password";
     public static final String REGISTER_SUCCESS_MESSSAGE = "Your Account Has Been Created";
-    public static final long DEFAULT_TIME_OUT = 5;
+    public static final long DEFAULT_TIME_OUT = 7;
     public static final long DEFAULT_POLLING_TIME = 500;
     public static final int IMAC_IMAGE_COUNT = 3;
     public static final int MACBOOKPRO_IMAGE_COUNT = 4;
@@ -25,7 +25,7 @@ public class ConstantValues
     public static final String REGISTER_SHEET_NAME = "Registartion";
     public static final String PRODUCT_SHEET_NAME = "Product";
     public static final String TEST_DATA_SHEET_PATH = ".\\src\\resources\\testdata\\opencarttestdata.xlsx";
-
+    
     public static List<String> getExpectedAccSecList()
     {
         List<String> expSecList = new ArrayList<String>();
